e64bbb9744c28b7a833ea6819cf9b89b323b0a80
Wahab Safdar <wahabsafdar@MBAir.lan>
2018-07-25 11:19:14 -0400

    initial commit

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
     res.send({Hi : "there"});
 });
 
-const PORT = process.env.PORT ; // whenever heroku runs application it can inject enviroment variable that are done in run time 
+const PORT = process.env.PORT || 5000 ; // whenever heroku runs application it can inject enviroment variable that are done in run time 
 app.listen(5000);
