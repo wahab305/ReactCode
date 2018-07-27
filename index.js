7112ba08b7996e46467a0b0f30d53a568f340bb3
Wahab Safdar <wahabsafdar@MBAir.local>
2018-07-26 21:50:57 -0400

    changed greeting

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const  express = require('express'); // require to get access to express library known as common Js Module
+require('./models/User');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI, {
+  useMongoClient : true
+});
+
 const app = express();  // express handles the http request
 
-app.get('/', (req, res) => {
-    res.send({bye : "buddy"});
-});
+require('./routes/authRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000 ; // whenever heroku runs application it can inject enviroment variable that are done in run time
 app.listen(PORT);
-
-
-/// bye
