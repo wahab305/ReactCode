66c1cccfc0ef490b86920ecd726cf108d29ae69d
Wahab Safdar <wahabsafdar@MBAir.lan>
2018-07-27 13:31:17 -0400

    tweak google

--- services/passport.js
+++ services/passport.js
@@ -2,28 +2,24 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
-
-
 const User = mongoose.model('users');
 
- passport.serializeUser((user, done) => {
-      done(null, user.id);
+       passport.serializeUser((user, done) => {
+       done(null, user.id);
  });
-
-passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
+        passport.deserializeUser((id, done) => {
+        User.findById(id).then(user => {
+        done(null, user);
   })
 });
 
 
 passport.use(
-
   new GoogleStrategy({
-
   clientID : keys.googleClientID,
   clientSecret : keys.googleClientSecret,
-  callbackURL : '/auth/google/callback'
+  callbackURL : '/auth/google/callback',
+  proxy: true
 },
 
 (accessToken, refreshToken, profile, done) => {
