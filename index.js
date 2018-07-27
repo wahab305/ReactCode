2a672e5ec164afc5db0e74c79089a33ec9b19342
Wahab Safdar <wahabsafdar@MBAir.lan>
2018-07-27 12:08:11 -0400

    finished auth

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
-require('./models/User');
+const User = require('./models/User');
 require('./services/passport');
 
 
