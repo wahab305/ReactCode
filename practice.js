cd2ffae6cc3edd0e5e0034e61275ede0e65bdf5f
Wahab Safdar <wahabsafdar@MBAir.lan>
2018-07-30 12:42:15 -0400

    fetchUser

---
+++ practice.js
@@ -0,0 +1,23 @@
+
+
+// function fetchAlbums() {
+//
+//   fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     .then( res => res.json())
+//     .then(json => console.log(json));
+// }
+// fetchAlbums();
+
+
+///Async wait
+
+const fetchAlbums = async () => {
+
+async function fetchAlbums() {
+
+const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+const json = await  res.json();
+ console.log(json);
+}
+
+fetchAlbums();
