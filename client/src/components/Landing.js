b13a462715439cca33eaf2e9e3704dcfa2295aff
Wahab Safdar <wahabsafdar@MBAir.lan>
2018-07-31 15:35:37 -0400

    header Done

---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Emaily!
+      </h1>
+      Collect feed back from Users
+      </div>
+  );
+};
+
+
+
+export default Landing;
